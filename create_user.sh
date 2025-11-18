b855f726740f504a68902330840b0737391b052d
RassulTokanov <rasul.tokanov@gmail.com>
2025-11-18 13:44:38 +0500

    Update create_user.sh

--- create_user.sh
+++ create_user.sh
@@ -1,5 +1,1 @@
-#!/bin/bash
-airflow db init
-airflow users create --username admin --firstname Admin --lastname User --role Admin --email rasul.tokanov@gmail.com --password admin || true
 
-exec airflow "$@"
